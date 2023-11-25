27422ed221b739b5d1cdc3dcf74dc91ba5bb44c7
cbhutad <learningcbhutad@gmail.com>
2023-11-25 18:03:30 +0530

    Sample codes

---
+++ StringTemplates/StringConcatenation.kt
@@ -0,0 +1,8 @@
+//StringConcatenation.kt
+
+fun main() {
+    val s: String = "hi\n"
+    val n: Int = 11
+    val d: Double = 3.14
+    println("first : " + s + " ,second : " + n + " ,third : " + d)
+}
