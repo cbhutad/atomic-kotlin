d897301cdddb0576b123ca98bc2d87c1f9081117
cbhutad <learningcbhutad@gmail.com>
2023-11-25 23:26:34 +0530

    Sample codes for NumberTypes atom

---
+++ NumberTypes/InferInt.kt
@@ -0,0 +1,6 @@
+//InferInt.kt
+
+fun main() {
+    val million = 1_000_000
+    println(million)
+}
---
+++ NumberTypes/IntDivisionTruncates.kt
@@ -0,0 +1,7 @@
+//IntDivisionTruncates.kt
+
+fun main() {
+    val numerator: Int = 19
+    val denominator: Int = 10
+    println(numerator / denominator)
+}
